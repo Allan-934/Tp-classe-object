186d0e76284670138033439bb93d2017e08675c3
Decugis Allan <allan.decugis@edouardgand.fr>
2026-03-30 16:40:16 +0200

    Exercice 2 v1

--- Program.cs
+++ Program.cs
@@ -4,8 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-       Produit p1 = new Produit("A001", "Produit 1", 10.5);
-       // Exercice 1  
-       Console.WriteLine(p1); 
+        Produit p1 = new Produit("A001", "Produit 1", 10.5);
+        // Exercice 1  
+        Console.WriteLine(p1);
+
+        // Exercice 2
+        Produit p2 = new Produit("A002", "Souris", 15.0);
+        Produit p3 = new Produit("A002", "Souris", 15.0);
+
+        Console.WriteLine(p2 == p3); 
+
+        
+
     }
 }
--- produit.cs
+++ produit.cs
@@ -1,20 +1,37 @@
-class Produit { 
-    private string codeArticle;  
-    private string designation; 
-    private double prix; 
- 
-    public Produit(string code, string nom, double px) { 
-        this.codeArticle = code; 
-        this.designation = nom; 
-        this.prix = px; 
-    } 
- 
-    public string GetCodeArticle() { 
-        return codeArticle; 
-    } 
+class Produit
+{
+    private string codeArticle;
+    private string designation;
+    private double prix;
+
+    public Produit(string code, string nom, double px)
+    {
+        this.codeArticle = code;
+        this.designation = nom;
+        this.prix = px;
+    }
+
+    public string GetCodeArticle()
+    {
+        return codeArticle;
+    }
 
     // Exercice 1
-    public override string ToString() { 
-        return $"Code Article: {codeArticle}, Designation: {designation}, Prix: {prix}"; 
+    public override string ToString()
+    {
+        return $"Code Article: {codeArticle}, Designation: {designation}, Prix: {prix}";
     }
-} 
+
+    // Exercice 2
+    public override bool Equals(object obj)
+    {
+        if (obj == null) return false;
+        if (obj is Produit autre)
+        {
+            return codeArticle == autre.codeArticle &&
+                   designation == autre.designation &&
+                   prix == autre.prix;
+        }
+        return false;
+    }
+}
